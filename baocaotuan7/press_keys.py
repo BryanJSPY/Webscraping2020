2eb6c9be55354bbecdd0309f545f3b2065f52398
BryanJSPY <thangdoanjspy@gmail.com>
2020-05-01 14:59:23 +0700

    Bao Cao Tuan 7

---
+++ baocaotuan7/press_keys.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+import time
+
+def ESC(driver):
+    time.sleep(1)
+    actions = ActionChains(driver)
+    actions.key_down(Keys.ESCAPE)
+    actions.key_up(Keys.ESCAPE)
+    actions.perform()
+
+
+def END(driver):
+    time.sleep(1)
+    actions = ActionChains(driver)
+    actions.key_down(Keys.END)
+    actions.key_up(Keys.END)
+    actions.perform()
+
+
+def CLICK(driver,ele):
+    time.sleep(1)
+    actions = ActionChains(driver)
+    actions.move_to_element(ele)
+    actions.click(ele)
+    actions.perform()
---
+++ baocaotuan7/scraping.py
@@ -0,0 +1,49 @@
+from my_driver import init_driver
+import login
+import press_keys
+from get_data_fb import get_all_posts_and_comments
+from sentiment import sentiment_all_comments
+from data_csv import write_all_posts_and_comments
+
+
+def get_error_on_driver(driver):
+    print("error")
+    driver.quit()
+
+
+def shut_down_driver(driver):
+    print("done")
+    driver.quit()
+
+
+if __name__ == "__main__":
+    while True:
+        driver = init_driver()
+        try:
+            all_id_en_posts = []
+            all_content_en_posts = []
+            all_users_replied_en_comment_each_en_posts = []
+            all_content_en_comment_each_en_posts = []
+            total_en_comment_each_en_posts = []
+            sentiment_all_en_comments = []
+
+            login.enter_username_password_login_fb(
+                driver, "dct99002@gmail.com", "vmax21399")
+
+            login.turn_off_popup(driver)
+
+            keyword = "ncovid19"
+            login.enter_keyword_to_search_on_fb(driver, keyword)
+
+            get_all_posts_and_comments(driver, all_id_en_posts, all_content_en_posts, total_en_comment_each_en_posts,
+                                       all_users_replied_en_comment_each_en_posts, all_content_en_comment_each_en_posts)
+            shut_down_driver(driver)
+
+            sentiment_all_comments(sentiment_all_en_comments,
+                                   all_content_en_comment_each_en_posts)
+            write_all_posts_and_comments(all_id_en_posts, all_content_en_posts,  total_en_comment_each_en_posts,
+                                         all_users_replied_en_comment_each_en_posts, all_content_en_comment_each_en_posts, sentiment_all_en_comments)
+            break
+        except Exception:
+            pass
+            get_error_on_driver(driver)
